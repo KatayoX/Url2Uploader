6deb2fc579a49a7f972b18f5436e6826a58a036a
KatayoX <97541762+KatayoX@users.noreply.github.com>
2022-06-04 21:23:28 +0530

    Update help_Nekmo_ffmpeg.py

--- helper_funcs/help_Nekmo_ffmpeg.py
+++ helper_funcs/help_Nekmo_ffmpeg.py
@@ -78,7 +78,7 @@ async def take_screen_shot(video_file, output_directory, ttl):
         "1",
         out_put_file_name
     ]
-     width = "90"
+    # width = "90"
     process = await asyncio.create_subprocess_exec(
         *file_genertor_command,
         # stdout must a pipe to be accessible as process.stdout
